49021e9d3a55e598a7cc889649b3d7178a3c5308
Mehmet Şükrü Ekemen <ekemenms@hotmail.com>
2022-06-17 12:20:48 +0300

    Generate Project Template

---
+++ app/main.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(int argc, const char* argv[])
+{
+    (void)fprintf(stdout, "%s", "Hi from the design patterns project..!");
+    
+    return 0;
+}
---
+++ tests/single_responsibility.cpp
@@ -0,0 +1,6 @@
+#include <gtest/gtest.h>
+
+TEST(SingleResponbilityTest, Dummy)
+{
+    ASSERT_TRUE(true);
+}
